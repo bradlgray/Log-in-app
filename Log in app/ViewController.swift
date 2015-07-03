2d72ac09d43d380ad766a932e2c63402802147de
Bradford Gray <bgray@westmont.edu>
2015-07-02 20:23:18 -0700

    Initial Commit

---
+++ Log in app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Log in app
+//
+//  Created by Brad Gray on 7/2/15.
+//  Copyright (c) 2015 Brad Gray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
