6d66308f0278226e2bbfe1fd00dce5b7869adc24
Bradford Gray <bgray@westmont.edu>
2015-07-02 20:48:57 -0700

    Basic Log in app

--- Log in app/ViewController.swift
+++ Log in app/ViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var Email: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+
+    @IBAction func Button(sender: UIButton) {
+        self.Email.resignFirstResponder()
+        self.password.resignFirstResponder()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        self.view.endEditing(true)
     }
-
-
 }
 
